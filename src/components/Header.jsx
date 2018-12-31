6872acfdebe3609c41e5a9dec4cc16952f055b44
wthomasdev <wlthomas001@gmail.com>
2018-12-31 16:49:27 -0700

    change point it to point it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getRoomName } from '../utils/url';
-import { ROOM } from '../constants/routes'
+import { ROOM } from '../constants/routes';
 
 export class Header extends Component {
   render() {
     const { pathname } = window.location;
     const isInRoom = pathname.includes(ROOM);
-    let roomName = isInRoom ? `Room: ${getRoomName()}` : 'Point It';
+    let roomName = isInRoom ? `Room: ${getRoomName()}` : 'Point It!';
 
     const backArrow =
       pathname !== '/' ? (
@@ -17,12 +17,11 @@ export class Header extends Component {
       ) : (
         <span />
       );
-    const hamburgerMenu =
-      isInRoom ? (
-        <span uk-toggle="target: #offcanvas-nav" uk-icon="icon: menu; ratio: 1.5" />
-      ) : (
-        <span />
-      );
+    const hamburgerMenu = isInRoom ? (
+      <span uk-toggle="target: #offcanvas-nav" uk-icon="icon: menu; ratio: 1.5" />
+    ) : (
+      <span />
+    );
 
     return (
       <header>
